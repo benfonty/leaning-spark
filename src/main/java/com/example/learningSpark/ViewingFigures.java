06d995df3ba954cc9ffe7f9a22acf45b950f13c5
benfonty <benfonty@gmail.com>
2022-06-15 16:05:05 +0200

    end big data exercise

--- src/main/java/com/example/learningSpark/ViewingFigures.java
+++ src/main/java/com/example/learningSpark/ViewingFigures.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import scala.Tuple2;
@@ -25,20 +26,55 @@ public class ViewingFigures {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         // Use true to use hardcoded data identical to that in the PDF guide.
-        boolean testMode = true;
+        boolean testMode = false;
 
         JavaPairRDD<Integer, Integer> viewData = setUpViewDataRdd(sc, testMode);
         JavaPairRDD<Integer, Integer> chapterData = setUpChapterDataRdd(sc, testMode);
         JavaPairRDD<Integer, String> titlesData = setUpTitlesDataRdd(sc, testMode);
 
-        chapterData
+        JavaPairRDD<Integer, Long> chaptersByCourses = chapterData
                 .mapToPair(Tuple2::swap)
                 .mapValues(x -> 1L)
-                .reduceByKey((x1, x2) -> x1 + x2)
-                .collect()
+                .reduceByKey((x1, x2) -> x1 + x2);
+
+        viewData = viewData.distinct();
+        //viewRDD(viewData);
+
+        JavaPairRDD<Integer, Tuple2<Integer, Integer>> toto1 = viewData.mapToPair(Tuple2::swap).join(chapterData);
+        //viewRDD(toto1);
+        JavaPairRDD<Integer, Tuple2<Long, Long>> toto2 = toto1
+                .mapToPair(Tuple2::swap)
+                .mapValues(x -> 1L)
+                .reduceByKey((v1, v2) -> v1 + v2)
+                .mapToPair(v -> new Tuple2<>(v._1._2, v._2))
+                .join(chaptersByCourses);
+        viewRDD(toto2);
+        toto2.mapValues(v -> {
+            double percentage = (v._1 * 100) / v._2;
+            if (percentage > 90) {
+                return 10;
+            } else if (percentage > 50) {
+                return 4;
+            } else if (percentage > 25) {
+                return 2;
+            } else {
+                return 0;
+            }
+        })
+                .reduceByKey((v1, v2) -> v1 + v2)
+                .join(titlesData)
+                .mapToPair(v-> new Tuple2<>(v._2._1, v._2._2))
+                .sortByKey(false)
+                .take(10)
                 .forEach(System.out::println);
 
         sc.close();
+    }
+
+    private static void viewRDD(JavaPairRDD<?, ?> rdd) {
+        System.out.println("RDD:");
+        rdd.collect().forEach(System.out::println);
+        System.out.println("");
     }
 
     private static JavaPairRDD<Integer, String> setUpTitlesDataRdd(JavaSparkContext sc, boolean testMode) {
